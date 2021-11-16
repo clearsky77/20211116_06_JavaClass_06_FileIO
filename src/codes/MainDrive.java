dce328f7dedfc97a3cb027e1a6458d57c4488eff
clearsky77 <clearsky3264@gmail.com>
2021-11-16 17:05:07 +0900

    파일 내용 불러오기 / 사용 끝나면 꼭 닫기!

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,15 +1,50 @@
 package codes;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class MainDrive {
 
 	public static void main(String[] args) {
-		writeToFile();
+		
+//		writeToFile(); //파일에 쓰기
+		readFile();
 	}
+	
+	static void readFile() {
+//		파일 LOAD (Input 실습)
+		
+//		1. 어디서 파일을 읽어올지? 경로 설정.
+		File myFile = new File("mytest.txt");
+		
+		try {
+			FileReader fr = new FileReader(myFile);
+			// 여러 글자를 뭉쳐서 String으로 읽어오기
+			BufferedReader br = new BufferedReader(fr); // fr을 도와서
+			
+			while(true) { // 횟수가 불명확하므로 while로 반복
+				String line = br.readLine();  // add catch lause 클릭해서 만든다!
+				if(line==null) {System.out.println("끝까지 읽음"); break;}
+				System.out.println(line);
+			}
+		// 사용이 끝나면 꼭 닫아주기!
+			br.close(); fr.close(); 
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	
 	static void writeToFile() {
 //		파일 SAVE (Output 실습)
